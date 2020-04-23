6a923c2743cf5568e59ab03ac737bee7d884b994
Henry-Saver <64166782+Henry-Saver@users.noreply.github.com>
2020-04-23 15:42:04 -0600

    Add files via upload

--- bST.cpp
+++ bST.cpp
@@ -12,7 +12,7 @@ bST::~bST() {
 }
 
 void bST::insert(int key) {
-    Node *temp;
+    Node *temp = new Node();
     temp->key = key;
     if (root == NULL) {
         root = temp;
@@ -28,11 +28,17 @@ void bST::insert(int key) {
                 current = current->right;
             }
         }
+        //current = temp;
+        //cout << "Setting new node: " << current->key << endl;
         if (key < prev->key) {
             prev->left = temp;
-        } else {
+            cout << "SETTING LEFT " << prev->key << " " << prev->left->key << endl;
+            } 
+        else {
             prev->right = temp;
-        }
+            cout << "SETTING RIGHT " << prev->key << " " << prev->right->key << endl;
+            }
+            
     }
 }
 
@@ -56,6 +62,31 @@ Node* bST::search(int key) {
     return recursiveSearch(key, root);
 }
 
-void bST::display() {
-    
+
+//This will print a visual model of the BST so that all node and their respective children/parent nodes can be seen
+void displayHelper(int n, Node* here)
+{
+    if(here == NULL) //Leaf check
+    {
+        return;
+    }
+    cout << here->key << endl;
+    displayHelper(n,here->left);
+    displayHelper(n,here->right);
+    // n = n + 10; //This increments the spacing on the model
+    // displayHelper(n,here->right); //Right is traversed before left // Right - Here - Left
+    // cout << endl;
+    // for(int i = 10;i < n;i++)
+    // {
+    //     printf(" ");
+    // }
+    // printf("%d\n", here->key);
+    // displayHelper(n,here->left);
+    return;
+}
+void bST::display() 
+{
+    int spaceNum = 1;
+    displayHelper(spaceNum, root);
+    return;
 }
