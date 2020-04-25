ff25f444250fb6afb10f57eb4cc112bdfe73e93d
Henry-Saver <64166782+Henry-Saver@users.noreply.github.com>
2020-04-24 21:44:52 -0600

    Add files via upload

---
+++ mainDriver.cpp
@@ -0,0 +1,220 @@
+#include <iostream>
+#include <fstream>
+#include "ctime"
+#include "linkedList.hpp"
+#include "bST.hpp"
+#include "linearProbing.hpp"
+#include "chaining.hpp"
+using namespace std;
+
+//load the data from a set from a chaining hash map
+double loadChaining(int data[], Chaining hashChain, int start, int end) {
+    clock_t t;
+    t = clock();
+    for (int i = start; i < end; i++) {
+        hashChain.insertItem(data[i]);
+    }
+    t = clock() - t;
+    return (double)t/CLOCKS_PER_SEC;
+}
+
+//search for all the items in the data set
+double searchChaining(int data[], int size, Chaining hashChain) {
+    clock_t t;
+    t = clock();
+    for (int i = 0; i < size; i++) {
+        hashChain.searchItem(data[i]);
+    }
+    t = clock() - t;
+    return (double)t/CLOCKS_PER_SEC;
+}
+
+//load data from a set for a linear probing hash map
+double loadLinearProbing(int data[], LinearProbing linearHash, int start, int end) {
+    clock_t t;
+    t = clock();
+    for (int i = start; i < end; i++) {
+        linearHash.insertItem(data[i]);
+    }
+    t = clock() - t;
+    return (double)t/CLOCKS_PER_SEC;
+}
+
+//search for all the items in the data set
+double searchLinearProbing(int data[], int size, LinearProbing linearHash) {
+    clock_t t;
+    t = clock();
+    for (int i = 0; i < size; i++) {
+        linearHash.searchItem(data[i]);
+    }
+    t = clock() - t;
+    return (double)t/CLOCKS_PER_SEC;
+}
+
+//load the data from a set for the linked list
+double loadLinkedList(int data[], LinkedList list, int start, int end) {
+    clock_t t;
+    t = clock();
+    for (int i = start; i < end; i++) {
+        list.insert(data[i]);
+    }
+    t = clock() - t;
+    return (double)t/CLOCKS_PER_SEC;
+}
+
+//search for all the items in the data set
+double searchLinkedList(int data[], int size, LinkedList list) {
+    clock_t t;
+    t = clock();
+    for (int i = 0; i < size; i++) {
+        list.search(data[i]);
+    }
+    t = clock() - t;
+    return (double)t/CLOCKS_PER_SEC;
+}
+
+//read the data for the binary search tree
+double loadBST(int data[], bST tree, int start, int end) {
+    clock_t t;
+    t = clock();
+    for (int i = start; i < end; i++) {
+        tree.insert(data[i]);
+    }
+    t = clock() - t;
+    return (double)t/CLOCKS_PER_SEC;
+}
+
+//search for all the items in the data set
+double searchBST(int data[], int size, bST tree) {
+    clock_t t;
+    t = clock();
+    for (int i = 0; i < size; i++) {
+        tree.search(data[i]);
+    }
+    t = clock() - t;
+    return (double)t/CLOCKS_PER_SEC;
+}
+
+
+
+int main() {
+    int dataSize = 40000; //data set is always constant size
+    int dataA[dataSize];
+    int dataB[dataSize];
+    ifstream file1;
+    ifstream file2;
+    file1.open("dataSetA.csv"); //read all of data set A into array A
+    if (file1.is_open()) {
+        string line;
+        for (int i = 0; i < 40000; i++) {
+            getline(file1, line, ',');
+            dataA[i] = stoi(line);
+        }
+    }
+    file1.close();
+    file2.open("dataSetB.csv"); // read all of data set B into array B
+    if (file2.is_open()) {
+        string line;
+        for (int i = 0; i < 40000; i++) {
+            getline(file2, line, ',');
+            dataB[i] = stoi(line);
+        }
+    }
+
+    //Declare all the data objects
+    Chaining hashChain(dataSize);
+    LinearProbing linearHash(dataSize);
+    LinkedList list;
+    bST tree;
+
+    //Data set A
+    double cInsertA[400];
+    double lPInsertA[400];
+    double lLInsertA[400];
+    double bSTInsertA[400];
+    double cSearchA[400];
+    double lPSearchA[400];
+    double bSTSearchA[400];
+
+    for (int i = 0; i < 400; i++) {
+        cInsertA[i] = loadChaining(dataA, hashChain, i * 100, (i * 100) + 100)/100;
+        lPInsertA[i] = loadLinearProbing(dataA, linearHash, i * 100, (i * 100) + 100)/100;
+        lLInsertA[i] = loadLinkedList(dataA, list, i * 100, (i * 100) + 100)/100;
+        bSTInsertA[i] = loadBST(dataA, tree, i * 100, (i * 100) + 100)/100;
+    }
+
+    hashChain.~Chaining();
+    linearHash.~LinearProbing();
+    list.~LinkedList();
+    tree.~bST();
+
+    //Data set B
+    double cInsertB[400];
+    double lPInsertB[400];
+    double lLInsertB[400];
+    double bSTInsertB[400];
+    double cSearchB[400];
+    double lPSearchB[400];
+    double bSTSearchB[400];
+
+    for (int i = 0; i < 400; i++) {
+        cInsertB[i] = loadChaining(dataA, hashChain, i * 100, (i * 100) + 100)/100;
+        lPInsertB[i] = loadLinearProbing(dataA, linearHash, i * 100, (i * 100) + 100)/100;
+        lLInsertB[i] = loadLinkedList(dataA, list, i * 100, (i * 100) + 100)/100;
+        bSTInsertB[i] = loadBST(dataA, tree, i * 100, (i * 100) + 100)/100;
+    }
+    ofstream mfile;
+    mfile.open("outputA.csv");
+    mfile << "A" << endl;
+    mfile << "Chaining" << endl;
+    for(int i = 0; i < 400;i++)
+    {
+        mfile << cInsertA[i] << ",";
+    }
+    mfile << endl;
+    mfile << "Linear Probing" << endl;
+    for(int i = 0; i < 400;i++)
+    {
+        mfile << lPInsertA[i] << ",";
+    }
+    mfile << endl;
+    mfile << "Linked List" << endl;
+    for(int i = 0; i < 400;i++)
+    {
+        mfile << lLInsertA[i] << ",";
+    }
+    mfile << endl;
+    mfile << "BST" << endl;
+    for(int i = 0; i < 400;i++)
+    {
+        mfile << bSTInsertA[i] << ",";
+    }
+    mfile.close();
+    mfile.open("outputB.csv");
+    mfile << "B" << endl;
+    mfile << "Chaining" << endl;
+    for(int i = 0; i < 400;i++)
+    {
+        mfile << cInsertB[i] << ",";
+    }
+    mfile << endl;
+    mfile << "Linear Probing" << endl;
+    for(int i = 0; i < 400;i++)
+    {
+        mfile << lPInsertB[i] << ",";
+    }
+    mfile << endl;
+    mfile << "Linked List" << endl;
+    for(int i = 0; i < 400;i++)
+    {
+        mfile << lLInsertB[i] << ",";
+    }
+    mfile << endl;
+    mfile << "BST" << endl;
+    for(int i = 0; i < 400;i++)
+    {
+        mfile << bSTInsertB[i] << ",";
+    }
+    mfile.close();
+
+}
